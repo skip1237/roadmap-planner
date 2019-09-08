9267f4172696311f177b55279dd8b936793bcdfc
Kamil Albrycht <ponciusz@gmail.com>
2019-09-08 22:07:25 +0100

    upgrade  eslint-utils

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
+import { firebase, db } from "../../firebase";
 
 // Utils
 // import { isLoggedIn } from "../../helpers/auth";
 
-const isLoggedIn = () => {
-  return false
-}
+// const isLoggedIn = () => {
+//   console.log("isLoggedIn");
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      isLoggedIn() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "login" }} />
-      )
-    }
-  />
-);
+//   firebase.auth.onAuthStateChanged(authUser => {
+//     if (authUser) {
+//       console.log("authUser", authUser);
+//       return true;
+//     } else {
+//       console.log("authUser is null");
+//       return false;
+//     }
+//   });
+//   return false;
+// };
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "login" }} />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
